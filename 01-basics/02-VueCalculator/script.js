4e7924d8020776b196708ce3e8bfd15d7bfc6b69
Pavel Bondarenok <pavel.bondarenok@gmail.com>
2022-06-21 23:13:03 +0300

    CounterButton and VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: null,
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'sum':
+          return this.num1 + this.num2;
+        case 'subtract':
+          return this.num1 - this.num2;
+        case 'multiply':
+          return this.num1 * this.num2;
+        case 'divide':
+          return this.num1 / this.num2;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+app.mount('#app');
