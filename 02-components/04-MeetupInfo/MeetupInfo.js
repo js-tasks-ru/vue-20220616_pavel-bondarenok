6f9138fc3e4ea1f8514180a3e6dd6e24bb83384c
Pavel Bondarenok <pavel.bondarenok@gmail.com>
2022-06-24 19:54:07 +0300

    Solved MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -2,20 +2,41 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
-
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    userLocaleLongDate() {
+      return new Date(this.$props.date).toLocaleDateString(navigator.language, { dateStyle: 'long' });
+    },
+    userLocaleISODate() {
+      return new Date(this.$props.date).toISOString().split('T')[0];
+    },
+  },
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="userLocaleISODate">{{ userLocaleLongDate }}</time>
       </li>
     </ul>`,
 });
