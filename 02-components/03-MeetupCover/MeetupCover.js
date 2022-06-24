5c2f3604faf7b5cd51c4668bb3a0b8a26694736c
Pavel Bondarenok <pavel.bondarenok@gmail.com>
2022-06-24 19:53:14 +0300

    Solved MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -2,9 +2,24 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
-
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    backgroundImage() {
+      if (!this.image) return '';
+      return `--bg-url: url(${this.image})`;
+    },
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="backgroundImage">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
