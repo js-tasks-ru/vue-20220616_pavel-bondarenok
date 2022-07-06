54d881511a36baf4ff9d2d77f455265a22aef06b
p_bandaronak <p_bandaronak@wargaming.net>
2022-07-06 22:22:07 +0300

    Solved AuthPages

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -2,5 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: () => import('../views/PageIndex.vue'),
+      name: 'main-page',
+    },
+    {
+      path: '/login',
+      component: () => import('../views/PageLogin.vue'),
+      name: 'login-form',
+    },
+    {
+      path: '/register',
+      component: () => import('../views/PageRegister.vue'),
+      name: 'register-form',
+    },
+  ],
 });
