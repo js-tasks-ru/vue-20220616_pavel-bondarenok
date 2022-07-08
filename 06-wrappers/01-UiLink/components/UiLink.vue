807e817c02e55935b49434ac5e61d6c5d7a70815
p_bandaronak <p_bandaronak@wargaming.net>
2022-07-08 14:58:11 +0300

    Solved UiLink

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,18 @@
 <template>
-  <a class="link">Link!</a>
+  <component :is="tag">
+    <a class="link"><slot/></a>
+  </component>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+  props: {
+    tag: {
+      type: String,
+      default: 'router-link',
+    },
+  },
 };
 </script>
 
